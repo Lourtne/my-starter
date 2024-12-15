d73767c2e85cf2c212dc2aab6dac5214ae3efd57
Sergey Komissarov <lourtne@gmail.com>
2024-12-15 16:50:52 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.html';
+import './styles/style.scss';
+
+import { sum, sub, mul, div } from './modules/module';
+
+console.log(sum(40, 2), sub(50, 8), mul(6, 7), div(84, 2))
+
+console.log(`It's working!`)
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        port: 9000,
+        open: false,
+        hot: true,
+    },
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        filename: '[name].[contenthash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[conttenthash].bundle.css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(eot|woff2?|[to]tf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+            {
+                test: /\.m?js$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ],
+    },
+};
